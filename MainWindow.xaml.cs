e1e355c861d6188a8cc4d7267c79cc8973d5f0fa
Paul K <pakummer@gmail.com>
2024-09-26 23:27:02 -0500

    added history and test all options

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Windows;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using PaulsRomanNumerals.JustusRomanNumClass;
@@ -26,11 +29,23 @@ namespace PaulsRomanNumerals
             { 500,   "D" },
             { 1000,  "M" }
         };
+        private CancellationTokenSource cts;
         #endregion
 
 
 
         #region "This is the main window's properties"
+        private ObservableCollection<History> _conversionHistory = new ObservableCollection<History>();
+        public ObservableCollection<History> ConversionHistory 
+        {
+            get => _conversionHistory;
+            set 
+            {
+                _conversionHistory = value;
+                OnPropertyChanged(nameof(ConversionHistory));
+            } 
+        } 
+
         private string _normalNumber = "0";
         public string NormalNumber
         {
@@ -91,13 +106,54 @@ namespace PaulsRomanNumerals
         {
             try
             {
-                // I bound directly to the property of the object, so this result isn't necessary.
-                var result = PaulNumeral.ConvertTheNumber(NormalNumber);
-                JustusNumeral.ConvertTheNumber();
+                int normalNumberInt = 0;
+                if(int.TryParse(NormalNumber, out normalNumberInt))
+                {
+                    NormalNumber = EnsureWithinValidRange(normalNumberInt);
+
+                    // I bound directly to the property of the object, so this result isn't necessary.
+                    var result = PaulNumeral.ConvertTheNumber(NormalNumber);
+                    JustusNumeral.ConvertTheNumber();
+
+                    Dispatcher.Invoke(() =>
+                    {
+                        ConversionHistory.Insert(0,new History(NormalNumber, PaulNumeral.RomanNumeral, JustusNumeral.RomanNumeral));
+                        OnPropertyChanged(nameof(ConversionHistory));
+                    } );
+                }
+                else
+                {
+                    MessageBox.Show("The provided string cannot be converter to a Roman Numeral", "Invalid Text", MessageBoxButton.OK, MessageBoxImage.Stop);
+                }
+                
             }
             catch (Exception ex)
             {
                 MessageBox.Show($"Some exception happened that I don't care about.{ex.ToString()}", "Somthing failed!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private String EnsureWithinValidRange(int numberToCheck)
+        {
+            int newNum = numberToCheck > 3999 ? 3999 : numberToCheck;
+            newNum = newNum < 0 ? 0 : newNum;
+            return newNum.ToString();
+        }
+
+        private async void TestAllNumbers()
+        {
+            cts = new CancellationTokenSource();
+            for (var num = 0; num <= 3999; num++)
+            {
+                if(cts.IsCancellationRequested)
+                {
+                    return;
+                }
+
+                NormalNumber = num.ToString();
+                await Task.Run(() => { ConvertNumbers(); });
+                
+                //await System.Threading.Tasks.Task.Delay(10);
             }
         }
         #endregion
@@ -116,6 +172,11 @@ namespace PaulsRomanNumerals
         {
             ConvertNumbers();
         }
+
+        private void TestAllNumbersButton_Click(object sender, RoutedEventArgs e)
+        {
+            TestAllNumbers();
+        }
         #endregion
 
         private void NormalNumber_TextBox_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
@@ -125,5 +186,24 @@ namespace PaulsRomanNumerals
                 ConvertNumbers();
             }
         }
+
+        private void StopButton_Click(object sender, RoutedEventArgs e)
+        {
+            cts.Cancel();
+        }
     }
+}
+
+public class History
+{
+    public History(string initial, string paul, string justus)
+    {
+        NumberToConvert = initial;
+        PaulVal = paul;
+        JustusVal = justus;
+    }
+
+    public string NumberToConvert { get; set; } = "NA";
+    public string PaulVal { get; set; } = "NA";
+    public string JustusVal { get; set; } = "NA";
 }
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("PaulsRomanNumerals")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Converts normal base 10 integers to their Roman equivelent")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("PaulsRomanNumerals")]
@@ -53,3 +53,4 @@ using System.Windows;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
